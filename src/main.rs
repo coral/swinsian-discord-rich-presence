b9b73867f1c78fb1f6f82ad7d6415588dcb2d1dc
coral <jonas@jonasbengtson.se>
2023-02-20 11:11:40 -0800

    dns

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use std::{
     thread::sleep,
     time::{Duration, Instant},
 };
+
+enum AppState {
+    Active,
+    Cleared,
+}
+
+#[allow(unreachable_code)]
 fn main() -> Result<(), error::SwinsianError> {
     println!("starting");
-    //let req = swinsian::get();
 
     let mut client = DiscordIpcClient::new("1076384656850698240")?;
     client.connect()?;
@@ -16,37 +22,35 @@ fn main() -> Result<(), error::SwinsianError> {
         if client.connect().is_ok() {
             break;
         }
-        sleep(Duration::from_secs(1));
+        sleep(Duration::from_secs(30));
         continue;
     }
 
-    // loop {
-    //     let payload = activity::Activity::new().state("Hello world!");
-    //     client.set_activity(payload)?;
-    //     sleep(Duration::from_secs(5));
-    // }
-
-    let mut has_closed = false;
+    let mut appstate = AppState::Active;
     let mut last_updated = Instant::now();
+
     loop {
-        if Instant::now().duration_since(last_updated).as_secs() >= 1 {
-            if update_presence(&mut client, &mut last_updated).is_ok() {
-                has_closed = false;
-                continue;
+        let data = swinsian::get()?;
+
+        match data.state {
+            swinsian::State::Playing => {
+                appstate = AppState::Active;
+                update_presence(data, &mut client, &mut last_updated)
             }
-        }
-        sleep(Duration::from_secs(1));
+            _ => clear(&mut client, &mut last_updated, &mut appstate),
+        }?;
+
+        sleep(Duration::from_secs(5));
     }
 
     Ok(())
 }
 
 fn update_presence(
+    data: swinsian::Swinsian,
     client: &mut impl DiscordIpc,
     last_updated: &mut Instant,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let data = swinsian::get()?;
-
+) -> Result<(), error::SwinsianError> {
     let state: String = format!("{} - {}", data.artist, data.album)
         .chars()
         .take(128)
@@ -62,12 +66,34 @@ fn update_presence(
         .details(&details)
         .assets(assets.clone());
 
-    if Instant::now().duration_since(*last_updated).as_secs() >= 5 {
+    if Instant::now().duration_since(*last_updated).as_secs() >= 4 {
         if client.set_activity(payload).is_err() {
             client.reconnect().ok();
         } else {
             *last_updated = Instant::now();
         }
+    }
+
+    Ok(())
+}
+
+fn clear(
+    client: &mut impl DiscordIpc,
+    last_updated: &mut Instant,
+    appstate: &mut AppState,
+) -> Result<(), error::SwinsianError> {
+    match appstate {
+        AppState::Active => {
+            if Instant::now().duration_since(*last_updated).as_secs() >= 4 {
+                if client.clear_activity().is_err() {
+                    client.reconnect().ok();
+                } else {
+                    *last_updated = Instant::now();
+                    *appstate = AppState::Cleared
+                }
+            }
+        }
+        _ => {}
     }
 
     Ok(())
--- src/swinsian.rs
+++ src/swinsian.rs
@@ -13,14 +13,29 @@ pub struct Request {
 #[serde(rename_all = "camelCase")]
 pub struct Swinsian {
     pub format: String,
-    pub state: String,
+    pub state: State,
     pub song: String,
     pub artist: String,
     pub album: String,
 }
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum State {
+    Playing,
+    Paused,
+    Stopped,
+    Unknown,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::Unknown
+    }
+}
+
 pub fn get() -> Result<Swinsian, SwinsianError> {
-    let r = Command::new("osascript").arg("as.scpt").output()?;
+    let r = Command::new("osascript").arg("as2.scpt").output()?;
 
     let p: Request = serde_json::from_slice(&r.stdout)?;
 
